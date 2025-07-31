e53319f3fcbc5fa02fe8e988d8024f55cd01fcee
Smaeroum <smahroum@gmail.com>
2025-07-31 09:43:51 +0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VisionRoomGame = () => {
+  return (
+    <div>
+      <h1>Welcome to the Vision Room Game!</h1>
+      <p>This is your prototype powered by React + Vite + Vercel.</p>
+    </div>
+  );
+};
+
+export default VisionRoomGame;
